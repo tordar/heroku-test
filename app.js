e95d0cdb62f89ecd24dc28a75ccbc861168432c0
tordar <tordar.tommervik@gmail.com>
2021-04-05 17:33:35 +0200

    Full treehouse app

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use('/static', express.static('public'))
+app.set('view engine', 'pug')
+
+const mainRoutes = require('./routes')
+const cardRoutes = require('./routes/cards')
+
+app.use(mainRoutes)
+app.use('/cards', cardRoutes)
+
+app.use((req, res, next) => {
+    const err = new Error('The page was not found')
+    err.status = 404;
+    next(err)
+})
+
+app.use((err, req, res, next) => {
+    res.locals.error = err
+    res.status(err.status)
+    res.render('error')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://oursite:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
